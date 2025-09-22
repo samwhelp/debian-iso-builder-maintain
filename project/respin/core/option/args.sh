c849f207e9dd5df1b4a3eb3818a91c2ab4c8f2b9
samwhelp <samwhelp@users.noreply.github.com>
2025-09-22 17:47:23 +0800

    fix

--- project/respin/core/option/args.sh
+++ project/respin/core/option/args.sh
@@ -22,4 +22,4 @@ REF_ENTITY_THEME_LIST="attach-themes style-maccity style-wincity theme-vimix the
 ## ## REF_ENTITY_DESKTOP_LIST
 ##
 
-REF_ENTITY_DESKTOP_LIST="xfce kde-plasma budgie mate cinnamon gnome-shell mate-with-compiz lxqt lxqt-with-kwin"
+REF_ENTITY_DESKTOP_LIST="xfce kde-plasma budgie mate cinnamon gnome-shell mate-with-compiz lxqt lxqt-with-kwin bspwm cwm fluxbox herbstluftwm hyprland i3wm icewm jwm labwc openbox pekwm spectrwm sway wayfire"
